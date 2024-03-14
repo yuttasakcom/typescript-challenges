a7c2bcb1711d3f553a4bc6e53eb392a910e76a90
Yuttasak(Yo)Pannawat <yuttasak.pan@sellsuki.com>
2024-03-14 15:30:20 +0700

    feat:🥷 (basic) count-occurrences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.test.ts
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.test.ts
@@ -4,4 +4,5 @@ test("Count Occurrences of a Character", () => {
   expect(countOccurrences("hello", "l")).toBe(2);
   expect(countOccurrences("programming", "m")).toBe(2);
   expect(countOccurrences("banana", "a")).toBe(3);
+  expect(countOccurrences("banana", "z")).toBe(0);
 });
