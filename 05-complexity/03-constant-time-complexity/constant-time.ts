937a6f90c21b0e666c78cc1aa08cf0eb2a6abd3b
Yuttasak(Yo)Pannawat <yuttasak.pan@sellsuki.com>
2024-03-14 15:49:28 +0700

    feat:🥷 (basic) complexity constant time

---
+++ 05-complexity/03-constant-time-complexity/constant-time.ts
@@ -0,0 +1,13 @@
+function accessElement(arr: number[], index: number): number {
+  return arr[index];
+}
+
+const arr1 = [1, 2, 3, 4, 5];
+console.time("Access Element 1");
+console.log(accessElement(arr1, 1));
+console.timeEnd("Access Element 1");
+
+const arr2 = Array.from({ length: 10000 }, (_, index) => index + 1);
+console.time("Access Element 2");
+console.log(accessElement(arr2, 1));
+console.timeEnd("Access Element 2");
